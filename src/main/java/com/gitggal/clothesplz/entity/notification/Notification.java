b2dc9bd8553b232e788475dd7c2844a738c92153
Junkov0 <devjuncloud@gmail.com>
2026-05-04 18:30:47 +0900

    feat: 알림 Entity 및 관련 DTO 작성

--- src/main/java/com/gitggal/clothesplz/entity/notification/Notification.java
+++ src/main/java/com/gitggal/clothesplz/entity/notification/Notification.java
@@ -1,15 +1,44 @@
 package com.gitggal.clothesplz.entity.notification;
 
 import com.gitggal.clothesplz.entity.base.BaseEntity;
+import com.gitggal.clothesplz.entity.user.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 알림 Entity
+ * 필드 구성
+ * - receiver      : 알림을 받을 사용자
+ * - title         : 알림 제목 (예: "새로운 의상 속성이 추가되었어요.")
+ * - content       : 알림 내용 (예: "내 의상에 [사이즈] 속성을 추가해보세요.")
+ * - level         : 알림 심각도 (INFO / WARNING / ERROR)
+ */
 @Entity
 @Table(name = "notifications")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Notification extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "receiver_id", nullable = false)
+  private User receiver;
+
+  @Column(nullable = false, length = 50)
+  private String title;
+
+  @Column(length = 100)
+  private String content;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 10)
+  private NotificationLevel level;
 }
--- src/main/java/com/gitggal/clothesplz/entity/notification/NotificationLevel.java
+++ src/main/java/com/gitggal/clothesplz/entity/notification/NotificationLevel.java
@@ -1,5 +1,18 @@
 package com.gitggal.clothesplz.entity.notification;
 
+/**
+ * 알림의 심각도를 나타내는 enum
+ *
+ * 필드 구성
+ * - INFO          : 일반 정보성의 알림 (예: 좋아요, 팔로우, DM 수신, 피드 등록)
+ * - WARNING       : 주의가 필요한 알림 (예: 권한 변경)
+ * - ERROR         : 오류 관련 알림 (시스템 오류로 인한 처리 실패 등)
+ */
 public enum NotificationLevel {
-    INFO, WARNING, ERROR
+
+    INFO,
+
+    WARNING,
+
+    ERROR
 }
