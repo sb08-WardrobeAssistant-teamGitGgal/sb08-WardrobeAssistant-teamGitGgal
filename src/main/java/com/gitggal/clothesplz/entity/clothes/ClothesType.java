7d146636a85bdce21c25318b135e4070852de23f
castle-bird <seongjo0501@gmail.com>
2026-05-04 17:15:27 +0900

    feat: Entity 작성

--- src/main/java/com/gitggal/clothesplz/entity/clothes/ClothesType.java
+++ src/main/java/com/gitggal/clothesplz/entity/clothes/ClothesType.java
@@ -1,5 +1,16 @@
 package com.gitggal.clothesplz.entity.clothes;
 
 public enum ClothesType {
-    TOP, BOTTOM, DRESS, OUTER, UNDERWEAR, ACCESSORY, SHOES, SOCKS, HAT, BAG, SCARF, ETC
+  TOP,
+  BOTTOM,
+  DRESS,
+  OUTER,
+  UNDERWEAR,
+  ACCESSORY,
+  SHOES,
+  SOCKS,
+  HAT,
+  BAG,
+  SCARF,
+  ETC
 }
--- src/main/java/com/gitggal/clothesplz/entity/profile/Profile.java
+++ src/main/java/com/gitggal/clothesplz/entity/profile/Profile.java
@@ -1,9 +1,18 @@
 package com.gitggal.clothesplz.entity.profile;
 
 import com.gitggal.clothesplz.entity.base.BaseUpdatableEntity;
+import com.gitggal.clothesplz.entity.user.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,5 +20,35 @@ import lombok.NoArgsConstructor;
 @Table(name = "profiles")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Profile extends BaseUpdatableEntity {
+
+  @OneToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "image_url")
+  private String imageUrl;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "gender", length = 10)
+  private Gender gender;
+
+  @Column(name = "birth_date")
+  private LocalDate birthDate;
+
+  @Column(name = "latitude")
+  private Double latitude;
+
+  @Column(name = "longitude")
+  private Double longitude;
+
+  @Column(name = "temp_sensitivity")
+  private Short tempSensitivity;
+
+  @Column(name = "grid_x")
+  private Integer gridX;
+
+  @Column(name = "grid_y")
+  private Integer gridY;
 }
