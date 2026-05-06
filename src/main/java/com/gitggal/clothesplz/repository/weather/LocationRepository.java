f878a0840b1774577053a31f345817f9c11d448f
seungowon00 <hsw62255@naver.com>
2026-05-06 11:34:17 +0900

    feat: 날씨,지역 레퍼지토리 추가

--- src/main/java/com/gitggal/clothesplz/repository/weather/LocationRepository.java
+++ src/main/java/com/gitggal/clothesplz/repository/weather/LocationRepository.java
@@ -3,7 +3,14 @@ package com.gitggal.clothesplz.repository.weather;
 import com.gitggal.clothesplz.entity.weather.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LocationRepository extends JpaRepository<Location, UUID> {
+
+    Optional<Location> findByGridXAndGridY(Integer gridX, Integer gridY);
+
+    Optional<Location> findByLatitudeAndLongitude(double latitude, double longitude);
+
+    Optional<Location> findByLocationNamesContaining(String locationName);
 }
