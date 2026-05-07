f57c4e53942a8eacd64886766ebacb1771d831cd
parkhamin <parkhamint@naver.com>
2026-05-07 15:50:54 +0900

    feat: 피드 수정 기능 구현

--- src/main/java/com/gitggal/clothesplz/service/feed/FeedService.java
+++ src/main/java/com/gitggal/clothesplz/service/feed/FeedService.java
@@ -2,9 +2,14 @@ package com.gitggal.clothesplz.service.feed;
 
 import com.gitggal.clothesplz.dto.feed.FeedCreateRequest;
 import com.gitggal.clothesplz.dto.feed.FeedDto;
+import com.gitggal.clothesplz.dto.feed.FeedUpdateRequest;
+import java.util.UUID;
 
 public interface FeedService {
 
   // 피드 등록
   FeedDto createFeed(FeedCreateRequest feedCreateRequest);
+
+  // 피드 수정
+  FeedDto updateFeed(UUID feedId, FeedUpdateRequest feedUpdateRequest);
 }
