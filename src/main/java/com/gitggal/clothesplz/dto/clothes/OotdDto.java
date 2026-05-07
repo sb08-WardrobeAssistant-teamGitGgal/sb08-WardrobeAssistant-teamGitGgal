ae2efb6f248a40f86fbb2b6bb746b34498812694
parkhamin <parkhamint@naver.com>
2026-05-07 14:29:28 +0900

    fix: OotdDto의 type 자료형 수정

--- src/main/java/com/gitggal/clothesplz/dto/clothes/OotdDto.java
+++ src/main/java/com/gitggal/clothesplz/dto/clothes/OotdDto.java
@@ -1,5 +1,6 @@
 package com.gitggal.clothesplz.dto.clothes;
 
+import com.gitggal.clothesplz.entity.clothes.ClothesType;
 import java.util.List;
 import java.util.UUID;
 
@@ -7,7 +8,7 @@ public record OotdDto(
     UUID clothesId,
     String name,
     String imageUrl,
-    String type,
+    ClothesType type,
     List<ClothesAttributeDefDto> attributes
 ) {
 
