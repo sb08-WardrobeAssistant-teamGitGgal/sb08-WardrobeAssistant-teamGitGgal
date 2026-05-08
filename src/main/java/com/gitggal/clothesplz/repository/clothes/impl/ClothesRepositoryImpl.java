c604d3d540be6c4cbb70392d9feebeea87a30592
castle-bird <seongjo0501@gmail.com>
2026-05-08 14:41:24 +0900

    feat: ClothesRepository 커스텀 인터페이스 및 구현체 추가
    - ClothesRepository, ClothesAttributeRepository, ClothesAttributeDefRepository에 커스텀 인터페이스 및 구현체 연동

---
+++ src/main/java/com/gitggal/clothesplz/repository/clothes/impl/ClothesRepositoryImpl.java
@@ -0,0 +1,9 @@
+package com.gitggal.clothesplz.repository.clothes.impl;
+
+import com.gitggal.clothesplz.repository.clothes.ClothesRepositoryCustom;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClothesRepositoryImpl implements ClothesRepositoryCustom {
+
+}
