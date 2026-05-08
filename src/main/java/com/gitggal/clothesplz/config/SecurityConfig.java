4558f84dd68296150c415d9e0fc0c9fa40ed2f06
yeeun000 <dldpdms0000@naver.com>
2026-05-08 10:35:13 +0900

    feat : 로그인 구현

--- src/main/java/com/gitggal/clothesplz/config/SecurityConfig.java
+++ src/main/java/com/gitggal/clothesplz/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.gitggal.clothesplz.config;
 
+import com.gitggal.clothesplz.security.LoginFailureHandler;
 import com.gitggal.clothesplz.security.SpaCsrfTokenRequestHandler;
+import com.gitggal.clothesplz.security.LoginSuccessHandler;
+import com.gitggal.clothesplz.security.jwt.JwtAuthenticationFilter;
 import java.util.List;
 import java.util.stream.IntStream;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -10,29 +14,47 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Slf4j
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain filterChain(
+      HttpSecurity http,
+      LoginSuccessHandler loginSuccessHandler,
+      LoginFailureHandler loginFailureHandler
+  ) throws Exception {
     http
         .csrf(csrf -> csrf
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler())
-        )
+            .csrfTokenRequestHandler(new SpaCsrfTokenRequestHandler()))
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .formLogin(login->login
+            .loginProcessingUrl("/api/auth/sign-in")
+            .usernameParameter("email")
+            .passwordParameter("password")
+            .successHandler(loginSuccessHandler)
+            .failureHandler(loginFailureHandler))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(HttpMethod.GET,"/api/auth/csrf-token").permitAll() //csrf 토큰 조회 허용
             .requestMatchers(HttpMethod.POST, "/api/users").permitAll() // 회원가입 허용
+            .requestMatchers(HttpMethod.POST,"/api/auth/sign-in").permitAll() // 로그인 허용
             .requestMatchers("/api/**").authenticated() // api 인증 필요
             .anyRequest().permitAll()
-        );
+        )
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);;
     return http.build();
 
   }
