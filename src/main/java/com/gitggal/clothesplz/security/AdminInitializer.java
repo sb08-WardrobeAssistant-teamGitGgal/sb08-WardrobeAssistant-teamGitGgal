47ee0d3cfe8fe4a67a53a03684daaebc974be20e
yeeun000 <dldpdms0000@naver.com>
2026-05-14 10:47:51 +0900

    feat : 관리자로 초기화 구현

---
+++ src/main/java/com/gitggal/clothesplz/security/AdminInitializer.java
@@ -0,0 +1,58 @@
+package com.gitggal.clothesplz.security;
+
+import com.gitggal.clothesplz.entity.profile.Profile;
+import com.gitggal.clothesplz.entity.user.User;
+import com.gitggal.clothesplz.entity.user.UserRole;
+import com.gitggal.clothesplz.repository.profile.ProfileRepository;
+import com.gitggal.clothesplz.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements ApplicationRunner {
+
+  @Value("${clothesplz.admin.username}")
+  private String name;
+
+  @Value("${clothesplz.admin.password}")
+  private String password;
+
+  @Value("${clothesplz.admin.email}")
+  private String email;
+
+  private final UserRepository userRepository;
+  private final ProfileRepository profileRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Override
+  public void run(ApplicationArguments args) {
+    log.info("[AdminInitializer] 관리자 계정 생성 요청");
+    if (userRepository.existsByEmail(email)) {
+      log.info("[AdminInitializer] 관리자 계정 생성 실패 : 이미 계정이 존재합니다.");
+      return;
+    }
+
+    String encodePassword = passwordEncoder.encode(password);
+
+    try {
+      User admin = new User(name, email, encodePassword);
+      admin.updateRole(UserRole.ADMIN);
+      userRepository.save(admin);
+
+      Profile profile = Profile.builder()
+          .user(admin)
+          .build();
+      profileRepository.save(profile);
+      log.info("[AdminInitializer] 관리자 계정 생성 완료");
+    } catch (Exception e) {
+      log.warn("[AdminInitializer] 관리가 계정 생성 실패 : message={}", e.getMessage());
+    }
+  }
+}
