e1ce4435cff7a45bf797eb8ad4b061c8d088fc19
castle-bird <seongjo0501@gmail.com>
2026-05-15 13:40:14 +0900

    refactor: 코드래빗 리뷰 반영 3

--- src/main/java/com/gitggal/clothesplz/dto/clothes/ClothesAttributeDefCreateRequest.java
+++ src/main/java/com/gitggal/clothesplz/dto/clothes/ClothesAttributeDefCreateRequest.java
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record ClothesAttributeDefCreateRequest(
-    @NotNull
     @NotBlank(message = "name은 공백일 수 없습니다.")
+    @Size(min = 1, max = 20, message = "name은 2자 이상 20자 이하로 입력해주세요.")
     String name,
 
     @NotNull
