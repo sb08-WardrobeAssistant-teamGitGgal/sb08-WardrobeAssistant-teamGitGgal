7d151ade6fe6fab19676bc1d15eb2b683e7fed80
yeeun000 <dldpdms0000@naver.com>
2026-05-18 16:43:04 +0900

    test : 유저 컨트롤러 테스트 리팩토링

--- src/test/java/com/gitggal/clothesplz/controller/user/UserControllerTest.java
+++ src/test/java/com/gitggal/clothesplz/controller/user/UserControllerTest.java
@@ -3,14 +3,11 @@ package com.gitggal.clothesplz.controller.user;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.verify;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -96,11 +93,11 @@ public class UserControllerTest {
 
   @Nested
   @DisplayName("회원가입")
-  class createUser {
+  class CreateUser {
 
     @Test
     @DisplayName("회원가입 성공")
-    void success_createUser() throws Exception {
+    void createUser_success() throws Exception {
       //given
       given(userService.create(any(UserCreateRequest.class))).willReturn(userDto);
 
@@ -112,12 +109,11 @@ public class UserControllerTest {
           .andExpect(status().isCreated())
           .andExpect(jsonPath("$.name").value("GitGit"))
           .andExpect(jsonPath("$.email").value("Git@git.git"));
-
     }
 
     @Test
     @DisplayName("회원가입 실패")
-    void create_User_Validation_Fail() throws Exception {
+    void createUser_fail_validation() throws Exception {
       // given
       UserCreateRequest invalidRequest = new UserCreateRequest(null, "Git@git.git", "git1234!");
 
@@ -126,24 +122,24 @@ public class UserControllerTest {
               .with(csrf())
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(invalidRequest)))
-          .andDo(print())
           .andExpect(status().isBadRequest());
     }
   }
 
   @Nested
   @DisplayName("비밀번호 변경")
-  class updatePassword {
+  class UpdatePassword {
 
     @Test
     @DisplayName("비밀번호 변경 성공")
-    void success_updatePassword() throws Exception {
+    void updatePassword_success() throws Exception {
       // given
       ChangePasswordRequest request = new ChangePasswordRequest("newPassword123!");
       ClothesUserDetails principal = new ClothesUserDetails(
           userDto,
           "encodedPassword"
       );
+
       // when & then
       mockMvc.perform(patch("/api/users/{userId}/password", userId)
               .with(user(principal))
@@ -151,13 +147,11 @@ public class UserControllerTest {
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(request)))
           .andExpect(status().isNoContent());
-
-      verify(userService).updatePassword(eq(userId), any(ChangePasswordRequest.class));
     }
 
     @Test
     @DisplayName("비밀번호 변경 실패")
-    void updatePassword_validation_fail() throws Exception {
+    void updatePassword_fail_validation() throws Exception {
       // given
       ChangePasswordRequest invalidRequest = new ChangePasswordRequest("");
       ClothesUserDetails principal = new ClothesUserDetails(
@@ -170,7 +164,6 @@ public class UserControllerTest {
               .with(csrf())
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(invalidRequest)))
-          .andDo(print())
           .andExpect(status().isBadRequest());
     }
   }
@@ -181,7 +174,7 @@ public class UserControllerTest {
 
     @Test
     @DisplayName("역할 변경 성공")
-    void updateRole_success_withAdminRole() throws Exception {
+    void updateRole_success() throws Exception {
       // given
       UserRoleUpdateRequest request = new UserRoleUpdateRequest(UserRole.ADMIN);
       UserDto adminDto = new UserDto(
@@ -202,12 +195,9 @@ public class UserControllerTest {
               .with(csrf())
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(request)))
-          .andDo(print())
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.id").value(userId.toString()))
           .andExpect(jsonPath("$.role").value("ADMIN"));
-
-      verify(userService).updateRole(eq(userId), any(UserRoleUpdateRequest.class));
     }
 
     @Test
@@ -225,11 +215,8 @@ public class UserControllerTest {
               .with(csrf())
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(request)))
-          .andDo(print())
           .andExpect(status().isNotFound())
           .andExpect(jsonPath("$.exceptionName").value(UserErrorCode.USER_NOT_FOUND.name()));
-
-      verify(userService).updateRole(eq(userId), any(UserRoleUpdateRequest.class));
     }
 
     @Test
@@ -244,31 +231,19 @@ public class UserControllerTest {
               .with(csrf())
               .contentType(MediaType.APPLICATION_JSON)
               .content(invalidRequest))
-          .andDo(print())
           .andExpect(status().isBadRequest());
     }
   }
 
   @Nested
   @DisplayName("계정 목록 조회")
-  class findAll {
+  class FindAll {
 
     @Test
     @DisplayName("계정 목록 조회 성공")
     void findAll_success() throws Exception {
 
       // given
-      UserDtoCursorRequest request = new UserDtoCursorRequest(
-          null,
-          null,
-          5,
-          "email",
-          "ASCENDING",
-          null,
-          null,
-          null
-      );
-
       UserDtoCursorResponse response = new UserDtoCursorResponse(
           List.of(userDto),
           null,
@@ -279,15 +254,13 @@ public class UserControllerTest {
           "ASCENDING"
       );
 
-      given(userService.findAll(eq(request))).willReturn(response);
+      given(userService.findAll(any(UserDtoCursorRequest.class))).willReturn(response);
 
       mockMvc.perform(get("/api/users")
-              .with(csrf())
               .param("limit", "5")
               .param("sortBy", "email")
               .param("sortDirection", "ASCENDING")
               .contentType(MediaType.APPLICATION_JSON))
-          .andDo(print())
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.data").isArray())
           .andExpect(jsonPath("$.data.length()").value(1))
@@ -295,8 +268,6 @@ public class UserControllerTest {
           .andExpect(jsonPath("$.totalCount").value(1))
           .andExpect(jsonPath("$.sortBy").value("email"))
           .andExpect(jsonPath("$.sortDirection").value("ASCENDING"));
-
-      verify(userService, times(1)).findAll(any(UserDtoCursorRequest.class));
     }
 
     @Test
@@ -304,17 +275,15 @@ public class UserControllerTest {
     void findAll_fail() throws Exception {
 
       mockMvc.perform(get("/api/users")
-              .with(csrf())
               .param("sortDirection", "ASCENDING")
               .contentType(MediaType.APPLICATION_JSON))
-          .andDo(print())
           .andExpect(status().isBadRequest());
     }
   }
 
   @Nested
   @DisplayName("계정 잠금 상태 변경")
-  class updateLock {
+  class UpdateLock {
 
     @Test
     @DisplayName("계정 잠금 상태 변경 성공")
@@ -340,8 +309,6 @@ public class UserControllerTest {
               .content(objectMapper.writeValueAsString(request)))
           .andExpect(status().isOk())
           .andExpect(jsonPath("$.locked").value(true));
-
-      verify(userService, times(1)).updateLock(eq(userId), any(UserLockUpdateRequest.class));
     }
 
     @Test
@@ -356,7 +323,8 @@ public class UserControllerTest {
               .with(csrf())
               .contentType(MediaType.APPLICATION_JSON)
               .content(objectMapper.writeValueAsString(request)))
-          .andExpect(status().isNotFound());
+          .andExpect(status().isNotFound())
+          .andExpect(jsonPath("$.exceptionName").value(UserErrorCode.USER_NOT_FOUND.name()));
     }
   }
 
