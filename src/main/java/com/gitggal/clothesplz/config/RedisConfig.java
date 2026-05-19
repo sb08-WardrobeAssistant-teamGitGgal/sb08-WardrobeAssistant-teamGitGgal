46b74f7ed743ce23f5d84523e9e86f3f42dd04c0
Junkov0 <devjuncloud@gmail.com>
2026-05-19 09:26:29 +0900

    feat: Redis 인프라 추가 설정

--- src/main/java/com/gitggal/clothesplz/config/RedisConfig.java
+++ src/main/java/com/gitggal/clothesplz/config/RedisConfig.java
@@ -1,7 +1,9 @@
 package com.gitggal.clothesplz.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -14,12 +16,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 /**
  * Redis 설정 파일
  */
+@Configuration
+@RequiredArgsConstructor
 public class RedisConfig  {
 
   @Value("${spring.data.redis.host}")
   private String host;
 
-  @Value("${spring.data.redis.port")
+  @Value("${spring.data.redis.port}")
   private int port;
 
   @Bean
