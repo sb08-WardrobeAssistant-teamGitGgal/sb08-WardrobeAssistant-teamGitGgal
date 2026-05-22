c2eed16c5d82a60116aaee32dd1766e7f9fbc383
castle-bird <seongjo0501@gmail.com>
2026-05-22 11:23:09 +0900

    feat: OpenSearch 인증 및 Content-Type 설정 통합
    - Elasticsearch Basic Auth 설정 추가 (username, password)
    - Content-Type 관련 Amazon OpenSearch 호환성 픽스와 기본 인증 설정을 하나의 콜백으로 통합
    - Authentication 설정과 Content-Type 충돌 방지

--- src/main/java/com/gitggal/clothesplz/config/OpenSearchConfig.java
+++ src/main/java/com/gitggal/clothesplz/config/OpenSearchConfig.java
@@ -1,6 +1,10 @@
 package com.gitggal.clothesplz.config;
 
 import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,19 +12,37 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenSearchConfig {
 
-    // Amazon OpenSearch는 'compatible-with=8' Content-Type 미지원
-    // 인터셉터로 application/json으로 교체
+    @Value("${spring.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.elasticsearch.password}")
+    private String password;
+
+    // DefaultRestClientBuilderCustomizer(Basic Auth)와 우리 Content-Type 픽스가
+    // 각각 setHttpClientConfigCallback을 호출하면 마지막 호출이 앞 것을 덮어씀.
+    // 두 설정을 하나의 콜백으로 통합해 충돌 방지.
     @Bean
     public RestClientBuilderCustomizer openSearchCompatibility() {
         return builder -> builder.setHttpClientConfigCallback(
-            httpClientBuilder -> httpClientBuilder.addInterceptorLast(
-                (HttpRequestInterceptor) (request, context) -> {
-                    request.removeHeaders("Content-Type");
-                    request.addHeader("Content-Type", "application/json");
-                    request.removeHeaders("Accept");
-                    request.addHeader("Accept", "application/json, */*");
-                }
-            )
+            httpClientBuilder -> {
+                BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+                credentialsProvider.setCredentials(AuthScope.ANY,
+                    new UsernamePasswordCredentials(username, password));
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+
+                // Amazon OpenSearch는 'compatible-with=8' Content-Type 미지원
+                httpClientBuilder.addInterceptorLast(
+                    (HttpRequestInterceptor) (request, context) -> {
+                        request.removeHeaders("Content-Type");
+                        request.addHeader("Content-Type", "application/json");
+                        request.removeHeaders("Accept");
+                        request.addHeader("Accept", "application/json, */*");
+                    }
+                );
+
+                return httpClientBuilder;
+            }
         );
     }
 }
